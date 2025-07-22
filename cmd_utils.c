078c34ca24a039729b23d60978fc12b94c786c72
jugarcia42 <jugarcia42telefonica@gmail.com>
2025-07-23 01:45:54 +0200

    Update cmd_utils.c

--- cmd_utils.c
+++ cmd_utils.c
@@ -38,29 +38,40 @@ static char	**get_paths(char **envp)
 	return (ft_split(envp[i] + 5, ':'));
 }
 
-char	*get_cmd_path(char *cmd, char **envp)
+char *get_cmd_path(char *cmd, char **envp)
 {
-	char	**paths;
-	char	*full;
-	int		i;
+    char **paths;
+    char *full;
+    int i;
+    int found_no_exec = 0;
 
-	paths = get_paths(envp);
-	if (!paths)
-		return (NULL);
-	i = 0;
-	while (paths[i])
-	{
-		full = join_path_cmd(paths[i], cmd);
-		if (!full)
-			break ;
-		if (access(full, X_OK) == 0)
-		{
-			ft_free_split(paths);
-			return (full);
-		}
-		free(full);
-		i++;
-	}
-	ft_free_split(paths);
-	return (NULL);
+    paths = get_paths(envp);
+    if (!paths)
+        return (NULL);
+
+    i = 0;
+    while (paths[i])
+    {
+        full = join_path_cmd(paths[i], cmd);
+        if (!full)
+            break ;
+        if (access(full, F_OK) == 0) // El archivo existe
+        {
+            if (access(full, X_OK) == 0) // El archivo es ejecutable
+            {
+                ft_free_split(paths);
+                return (full);
+            }
+            else
+                found_no_exec = 1; // Archivo existe pero sin permiso
+        }
+        free(full);
+        i++;
+    }
+    ft_free_split(paths);
+
+    if (found_no_exec)
+        return (ft_strdup("NO_EXEC_PERMISSION"));
+    return (NULL);
 }
+

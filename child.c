aace799821d306699d175c75d8581ab7f0f86a92
jugarcia42 <jugarcia42telefonica@gmail.com>
2025-07-23 02:24:15 +0200

    Create child.c

---
+++ child.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jugarcia <jugarcia@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 18:26:41 by jugarcia          #+#    #+#             */
+/*   Updated: 2025/07/09 18:26:41 by jugarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+void	child_one(int *fd, char **argv, char **envp)
+{
+	int	infile;
+
+	infile = open(argv[1], O_RDONLY);
+	if (infile < 0)
+	{
+		perror("infile");
+		exit(1);
+	}
+	dup2(infile, 0);
+	dup2(fd[1], 1);
+	close(fd[0]);
+	close(fd[1]);
+	close(infile);
+	execute_cmd(argv[2], envp);
+}
+
+void	child_two(int *fd, char **argv, char **envp)
+{
+	int	outfile;
+
+	outfile = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (outfile < 0)
+	{
+		perror("outfile");
+		exit(1);
+	}
+	dup2(fd[0], 0);
+	dup2(outfile, 1);
+	close(fd[1]);
+	close(fd[0]);
+	close(outfile);
+	execute_cmd(argv[3], envp);
+}

e83d55eb9a86d4ebe0daa80cd744e4ebcdab454c
jugarcia42 <jugarcia42telefonica@gmail.com>
2025-07-23 02:36:31 +0200

    Update pipex.h

--- pipex.h
+++ pipex.h
@@ -23,16 +23,21 @@
 
 void	ft_free_split(char **arr);
 
-void	execute_cmd(char *cmd_str, char **envp);
 char	*get_cmd_path(char *cmd, char **envp);
+
+void	execute_cmd(char *cmd_str, char **envp);
+
+void	child_one(int *fd, char **argv, char **envp);
+void	child_two(int *fd, char **argv, char **envp);
+
+pid_t	create_child_one(int *fd, char **argv, char **envp);
+pid_t	create_child_two(int *fd, char **argv, char **envp);
+int		setup_and_fork(int *fd, char **argv, char **envp);
 
 void	wait_and_check(pid_t pid, char *cmd);
 void	handle_wait(int status, char *cmd);
 
-int		setup_and_fork(int *fd, char **argv, char **envp);
-void	child_one(int *fd, char **argv, char **envp);
-void	child_two(int *fd, char **argv, char **envp);
-
 #endif
+
 
 

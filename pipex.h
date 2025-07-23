4a1d9aa7582b0276acb4d6d2788035ddeab543d9
jugarcia42 <jugarcia42telefonica@gmail.com>
2025-07-23 02:56:31 +0200

    Update pipex.h

--- pipex.h
+++ pipex.h
@@ -18,24 +18,10 @@
 # include <fcntl.h>
 # include <sys/wait.h>
 # include <stdio.h>
-
 # include "libft/libft.h"
 
-char	*get_cmd_path(char *cmd, char **envp);
-
-void	execute_cmd(char *cmd_str, char **envp);
-
-void	child_one(int *fd, char **argv, char **envp);
-void	child_two(int *fd, char **argv, char **envp);
-
-pid_t	create_child_one(int *fd, char **argv, char **envp);
-pid_t	create_child_two(int *fd, char **argv, char **envp);
-int		setup_and_fork(int *fd, char **argv, char **envp);
-
-void	wait_and_check(pid_t pid, char *cmd);
-void	handle_wait(int status, char *cmd);
+void    execute_cmd(char *cmd_str, char **envp);
+char    *get_cmd_path(char *cmd, char **envp);
+int     setup_and_fork(int *fd, char **argv, char **envp);
 
 #endif
-
-
-

b4def395e6cf5d3f95710a6a52a19c7f06c289ec
jugarcia42 <jugarcia42telefonica@gmail.com>
2025-07-23 02:47:19 +0200

    Create ft_strcmp.c

---
+++ libft/ft_strcmp.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jugarcia <jugarcia@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 18:26:41 by jugarcia          #+#    #+#             */
+/*   Updated: 2025/07/09 18:26:41 by jugarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_strcmp(const char *s1, const char *s2)
+{
+    size_t i = 0;
+
+    while (s1[i] && s2[i] && s1[i] == s2[i])
+        i++;
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}

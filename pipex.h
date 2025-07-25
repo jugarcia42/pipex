adb1d569db8fa81f7762f90a18450dd12ed8025b
jugarcia42 <jugarcia42telefonica@gmail.com>
2025-07-25 21:43:53 +0200

    Update pipex.h

--- pipex.h
+++ pipex.h
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jugarcia <jugarcia@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: jugarcia <jugarcia@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 18:26:41 by jugarcia          #+#    #+#             */
 /*   Updated: 2025/07/09 18:26:41 by jugarcia         ###   ########.fr       */
@@ -20,11 +20,11 @@
 # include <stdio.h>
 # include "libft/libft.h"
 
-void    execute_cmd(char *cmd_str, char **envp);
-char    *get_cmd_path(char *cmd, char **envp);
-int     setup_and_fork(int *fd, char **argv, char **envp);
-void    child_one(int *fd, char **argv, char **envp);
-void    child_two(int *fd, char **argv, char **envp);
-void    wait_and_check(pid_t pid, char *cmd);
+void	execute_cmd(char *cmd_str, char **envp);
+char	*get_cmd_path(char *cmd, char **envp);
+int		setup_and_fork(int *fd, char **argv, char **envp);
+int		wait_and_check(pid_t pid, char *cmd);
+void	child_one(int *fd, char **argv, char **envp);
+void	child_two(int *fd, char **argv, char **envp);
 
 #endif
